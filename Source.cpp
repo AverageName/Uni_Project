1e0895317826cc4e6b0d918c488bb2c15f67356a
Idk <30757789+AverageName@users.noreply.github.com>
2018-03-13 20:53:53 +0300

    13.03

--- Source.cpp
+++ Source.cpp
@@ -25,15 +25,26 @@ int main()
 			kek3[i][j] = i;
 		}
 	}
+	double** kek4 = new double*[4];
+	for (int i = 0; i < 4; ++i)
+	{
+		kek4[i] = new double[1];
+		for (int j = 0; j < 1; ++j)
+		{
+			kek4[i][j] = i * 3 + 1;
+		}
+	}
 
 	Matrix kek1(kek, 4, 3);
 	Matrix kek2(kek3, 3, 1);
+	Matrix kek5(kek4, 4, 1);
 
 	cout << kek1 << endl;
-	cout << kek1 << endl;
+	cout << kek2 << endl;
+	cout << kek5 << endl;
 	cout << forward_prop(kek1,kek2) << endl;
 
-	cout << (kek1 + 5.25) << endl;
+	cout << back_prop(kek1,kek2,kek5,0.1) << endl;
 
 	for (int i = 0; i < 4; ++i)
 	{
@@ -46,6 +57,12 @@ int main()
 		delete[] kek3[i];
 	}
 	delete[] kek3;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		delete[] kek4[i];
+	}
+	delete kek4;
 	system("pause");
 	return 0;
 }
