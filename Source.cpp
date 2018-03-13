eeaf215817725d910037684f4e6012d5ebd0b208
Idk <30757789+AverageName@users.noreply.github.com>
2018-03-13 17:34:00 +0300

    Source

--- Source.cpp
+++ Source.cpp
@@ -19,18 +19,21 @@ int main()
 	double** kek3 = new double*[3];
 	for (int i = 0; i < 3; ++i)
 	{
-		kek3[i] = new double[4];
-		for (int j = 0; j < 4; ++j)
+		kek3[i] = new double[1];
+		for (int j = 0; j < 1; ++j)
 		{
 			kek3[i][j] = i;
 		}
 	}
 
 	Matrix kek1(kek, 4, 3);
-	Matrix kek2(kek3, 3, 4);
+	Matrix kek2(kek3, 3, 1);
+
 	cout << kek1 << endl;
 	cout << kek1 << endl;
-	cout << (kek2*kek1) << endl;
+	cout << forward_prop(kek1,kek2) << endl;
+
+	cout << (kek1 + 5.25) << endl;
 
 	for (int i = 0; i < 4; ++i)
 	{
