dba55c4d06eb172aa67b3ed96b8e348339565f5a
Idk <30757789+AverageName@users.noreply.github.com>
2018-03-14 10:49:01 +0300

    14.03

--- Matrices.h
+++ Matrices.h
@@ -9,27 +9,42 @@ private:
 	int rows;
 	int cols;
 public:
+
+	//Constructors
 	Matrix();
-	Matrix(double** _data,int _rows, int _cols);
 	Matrix(int rows, int cols);
+	Matrix(double** _data,int _rows, int _cols);
 	Matrix(const Matrix&);
+	//Destructor
 	~Matrix();
-	friend ostream& operator<<(ostream& out, const Matrix& matrix);
-	Matrix& operator=(const Matrix& matrix);
-	friend Matrix operator+(const Matrix& first_matrix, const Matrix& second_matrix);
-	friend Matrix operator*(const Matrix&, const Matrix&);
-	Matrix& operator*=(const Matrix&);
-	Matrix& operator+=(const Matrix&);
+	//Space allocation
 	void allocSpace();
-	friend Matrix operator+(const Matrix&,double);
-	friend Matrix forward_prop(const Matrix&, const Matrix&);
+	//Operator overloading
+	Matrix& operator=(const Matrix& matrix);
+	Matrix& operator+=(const Matrix&);
 	Matrix& operator-=(const Matrix& matrix);
+	Matrix& operator*=(const Matrix&);
+	
+	
+	friend Matrix operator+(const Matrix& first_matrix, const Matrix& second_matrix);
+	friend Matrix operator-(const Matrix& first_matrix, const Matrix& second_matrix);
+	friend Matrix operator*(const Matrix&, const Matrix&);
+	friend Matrix operator+(const Matrix&,double);
+	friend Matrix operator*(const Matrix&, const double);
+	friend ostream& operator<<(ostream& out, const Matrix& matrix);
+	
+	//Matrix functions
 	Matrix& Matrix::transpose(void);
-	friend Matrix operator*(const Matrix&,const double);
+	Matrix& Matrix::pow_elem(int degree);
 	double sum(void);
-	friend Matrix back_prop(Matrix& first_matrix, Matrix& weights, Matrix& true_labels,double);
-	friend void net_train(Matrix& first_matrix, Matrix& weights, Matrix& true_labels, double learning_rate, int num_iter);
-	friend double net_loss(Matrix& first_matrix, Matrix& weights, Matrix& true_labels);
+	
+	//Net functions
+	friend Matrix forward_prop(const Matrix&, const Matrix&, double);
+	friend double net_loss(Matrix& first_matrix, Matrix& weights, Matrix& true_labels, double&);
+	friend void back_prop(Matrix& first_matrix, Matrix& weights, Matrix& true_labels,double,double&);
+	friend void net_train(Matrix& first_matrix, Matrix& weights, Matrix& true_labels, double learning_rate,double& bias,int num_iter);
+	
+	
 
 
 
