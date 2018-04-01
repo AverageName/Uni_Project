e9969e453f58f33cc6bc72ce82787a6e0cec93e3
Idk <30757789+AverageName@users.noreply.github.com>
2018-04-01 11:04:04 +0300

    Update Matrices.h

--- Matrices.h
+++ Matrices.h
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <sstream>
+
 using namespace std;
 
 
@@ -45,9 +49,75 @@ public:
 	friend void net_train(Matrix& first_matrix, Matrix& weights, Matrix& true_labels, double learning_rate,double& bias,int num_iter);
 	
 	
-
+	/*friend double* max(const Matrix&);*/
 
 
 };
 
+class Module
+{
+public:
+	int submodel_num;
+	Module() { submodel_num = 0; }
+	~Module(){}
+	string module_name = "Module";
+	//Module* Ptr;
+	virtual Matrix forward(Matrix input) { return input; }
+	vector<pair<string,Module>> modules;
+	map<string, Matrix> parametres;
+	map<string, Matrix> grads;
 
+	void add_module(string module_name,Module module)
+	{
+		modules.push_back(pair<string, Module>(module_name,module));
+	}
+	
+	void add(Module module)
+	{
+		string module_name = to_string(submodel_num);
+		add_module(module_name, module);
+		submodel_num++;
+	}
+};
+
+
+class Sequential : public Module
+{
+public:
+	Sequential() { module_name = "Sequential"; }
+	~Sequential(){}
+	Matrix forward(Matrix input)
+	{
+		Matrix result = input;
+		for (auto submodules : modules)
+		{
+			result = submodules.second.forward(result);
+			
+		}
+		return result;
+	}
+
+};
+
+class Linear : public Module
+{
+public:
+	int input_size;
+	int output_size;
+	double bias;
+	Linear(int _input_size, int _output_size, double _bias)
+	{
+		input_size = _input_size;
+		output_size = _output_size;
+		bias = _bias;
+		parametres["weight"] = Matrix(input_size, output_size);
+	}
+	~Linear() {}
+
+	Matrix forward(Matrix input)
+	{
+		Matrix result = input;
+		result = input*parametres["weight"] + bias;
+		return result;
+	}
+};
